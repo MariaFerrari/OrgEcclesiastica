08836600660137c67b3014a5a473bc39f0fe945c
elisabettacapellini <elisabetta.capellini.studenti@isii.it>
2022-05-21 14:05:45 +0200

    Modifica diacono/index
    Modificata dashboard in diacono/index

--- orgeccl/app/views/diacono/index.php
+++ orgeccl/app/views/diacono/index.php
@@ -1,38 +1,91 @@
 <?php
 global $data;
 ?>
-<style>
-.button {
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 10px;
-  cursor: pointer;
-  width: 45%;
-  height: 200px;
-  background-color: gold;
 
-}
-.button:hover {
-  background-color: #4CAF50;
-  color: white;
-}
+<p style="margin:0.36em 0em 1.43em 0em;">
+    <span style="font-family:'Abel','Arial';font-size:1.714em;">Home - Diacono</span>
+</p>
 
-.button1 {background-color: #4CAF50;} /* Green */
-.button2 {background-color: #008CBA;} /* Blue */
-</style>
-</head>
-<body>
-    
-<div align="center">
-<button class="button">Funzione con maggior numero di partecipanti</button>
-<button class="button">Persone con genitori di parrocchie diverse</button>
-</div>
-<div align="center">
-<button class="button">Parrocchie con la puzza sotto il naso</button>
-<a href="<?=$data['base_path'];?>/diacono/scegli"><button class="button">Funzioni celebrate da un vescovo in una parrocchia</button></a>
-</div>
+<p style="margin:0.36em 0em 0.71em 0em;line-height:2.11267605633803;">
+    <div class="post_column col-lg-6 col-md-12 col-sm-12 col-xs-12">
+        <div class="ttr_Home_html_column42">
+            <div style="height:0px;width:0px;overflow:hidden;-webkit-margin-top-collapse: separate;"></div>
+            <div class="html_content">
+                <p style="margin:0.36em 0em 2.14em 0em;text-align:Center;">
+                    <span class="ttr_image" style="float:none;display:block;text-align:center;overflow:hidden;margin:0em 0em 2.86em 0em;">
+                        <span><img class="ttr_uniform" src="/orgeccl/assets/images/1.png" style="max-width:800px;max-height:479px;" /></span>
+                    </span>
+                    <span style="font-family:'Abel','Arial';font-size:1.429em;">RICERCA FUNZIONI</span>
+                    </p>
+                    <p style="margin:0.36em 0em 1.43em 0em;text-align:Center;line-height:2.11267605633803;">
+                    <span style="font-family:'Abel','Arial';font-size:1.143em;">Funzione con maggior numero di partecipanti.</span></p><p style="margin:0.36em 0em 0em 0em;text-align:Center;line-height:2.11267605633803;">
+                    <span><a HREF="" target="_self" class="btn btn-md btn-default">Vai alla pagina</a></span>
+                </p>
+            </div>
+            <div style="height:0px;width:0px;overflow:hidden;-webkit-margin-top-collapse: separate;"></div>
+            <div style="clear:both;"></div>
+        </div>
+    </div>
+    <div class="clearfix visible-sm-block visible-md-block visible-xs-block"></div>
+    <div class="post_column col-lg-6 col-md-12 col-sm-12 col-xs-12">
+        <div class="ttr_Home_html_column43">
+            <div style="height:0px;width:0px;overflow:hidden;-webkit-margin-top-collapse: separate;"></div>
+            <div class="html_content">
+                <p style="margin:0.36em 0em 2.14em 0em;text-align:Center;">
+                    <span class="ttr_image" style="float:none;display:block;text-align:center;overflow:hidden;margin:0em 0em 2.86em 0em;">
+                        <span><img class="ttr_uniform" src="/orgeccl/assets/images/0.png" style="max-width:800px;max-height:479px;" /></span>
+                    </span>
+                    <span style="font-family:'Abel','Arial';font-size:1.429em;">VISUALIZZA VESCOVI</span>
+                </p>
+                <p style="margin:0.36em 0em 2.14em 0em;text-align:Center;line-height:2.11267605633803;">
+                    <span style="font-family:'Abel','Arial';font-size:1.143em;">Persone con genitori di parrocchie diverse.</span></p><p style="margin:0.36em 0em 0em 0em;text-align:Center;line-height:2.11267605633803;">
+                    <span><a HREF="" target="_self" class="btn btn-md btn-default">Vai alla pagina</a></span>
+                </p>
+            </div>
+            <div style="height:0px;width:0px;overflow:hidden;-webkit-margin-top-collapse: separate;"></div>
+            <div style="clear:both;"></div>
+        </div>
+    </div>    
+</p>
+
+<p style="margin:0.36em 0em 0.71em 0em;line-height:2.11267605633803;">
+    <div class="post_column col-lg-6 col-md-12 col-sm-12 col-xs-12">
+        <div class="ttr_Home_html_column42">
+            <div style="height:0px;width:0px;overflow:hidden;-webkit-margin-top-collapse: separate;"></div>
+            <div class="html_content">
+                <p style="margin:0.36em 0em 2.14em 0em;text-align:Center;">
+                    <span class="ttr_image" style="float:none;display:block;text-align:center;overflow:hidden;margin:0em 0em 2.86em 0em;">
+                        <span><img class="ttr_uniform" src="/orgeccl/assets/images/1.png" style="max-width:800px;max-height:479px;" /></span>
+                    </span>
+                    <span style="font-family:'Abel','Arial';font-size:1.429em;">RICERCA FUNZIONI</span>
+                    </p>
+                    <p style="margin:0.36em 0em 1.43em 0em;text-align:Center;line-height:2.11267605633803;">
+                    <span style="font-family:'Abel','Arial';font-size:1.143em;">Parrocchie con la puzza sotto il naso.</span></p><p style="margin:0.36em 0em 0em 0em;text-align:Center;line-height:2.11267605633803;">
+                    <span><a HREF="" target="_self" class="btn btn-md btn-default">Vai alla pagina</a></span>
+                </p>
+            </div>
+            <div style="height:0px;width:0px;overflow:hidden;-webkit-margin-top-collapse: separate;"></div>
+            <div style="clear:both;"></div>
+        </div>
+    </div>
+    <div class="clearfix visible-sm-block visible-md-block visible-xs-block"></div>
+    <div class="post_column col-lg-6 col-md-12 col-sm-12 col-xs-12">
+        <div class="ttr_Home_html_column43">
+            <div style="height:0px;width:0px;overflow:hidden;-webkit-margin-top-collapse: separate;"></div>
+            <div class="html_content">
+                <p style="margin:0.36em 0em 2.14em 0em;text-align:Center;">
+                    <span class="ttr_image" style="float:none;display:block;text-align:center;overflow:hidden;margin:0em 0em 2.86em 0em;">
+                        <span><img class="ttr_uniform" src="/orgeccl/assets/images/0.png" style="max-width:800px;max-height:479px;" /></span>
+                    </span>
+                    <span style="font-family:'Abel','Arial';font-size:1.429em;">VISUALIZZA VESCOVI</span>
+                </p>
+                <p style="margin:0.36em 0em 2.14em 0em;text-align:Center;line-height:2.11267605633803;">
+                    <span style="font-family:'Abel','Arial';font-size:1.143em;">Funzioni celebrate da un vescovo in una parrocchia.</span></p><p style="margin:0.36em 0em 0em 0em;text-align:Center;line-height:2.11267605633803;">
+                    <span><a HREF="<?=$data['base_path'];?>/diacono/scegli" target="_self" class="btn btn-md btn-default">Vai alla pagina</a></span>
+                </p>
+            </div>
+            <div style="height:0px;width:0px;overflow:hidden;-webkit-margin-top-collapse: separate;"></div>
+            <div style="clear:both;"></div>
+        </div>
+    </div>    
+</p>
